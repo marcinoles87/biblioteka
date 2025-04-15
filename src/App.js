e16361a4579ef283f77a65144ee02ffbb9d6677d
marcinoles87 <marcinoles87@gmail.com>
2025-04-15 16:24:51 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+
+        <Route path='*' element={
+          <>
+          <h1>Biblioteka 2024</h1>
+          <Nav></Nav>
+          <Header></Header>
+          <Main></Main>
+          <SearchBar></SearchBar>
+          </>
+        }>
+
+        </Route>
+
+
+      </Routes>
+      
     </div>
   );
 }
