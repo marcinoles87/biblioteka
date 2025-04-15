7830aedada894b9b7d88cfcfd53e06fa43cf7e4d
marcinoles87 <marcinoles87@gmail.com>
2025-04-15 20:05:53 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,59 @@ import Nav from './components/Nav';
 import Header from './components/Header';
 import Main from './components/Main';
 import SearchBar from './components/SearchBar';
+import dataFile from '../src/backend/data.txt';
+import { useState } from 'react';
+
 
 
 function App() {
+
+  
+
+  const [datas , setDatas] = useState('')
+
+
+    const handleFileRead = (e) =>{
+
+      const file = e.target.files[0]
+      const reader = new FileReader();
+      reader.readAsText(file)
+
+
+  reader.onload = (e) => {
+
+    const respo = e.target.result
+
+    // setDatas(file)
+      console.log(respo)
+
+      
+      setDatas(reader.result)
+    }
+
+    
+
+   
+
+  }
+
+
   return (
     <div className="App">
+
+<input type="file" onChange={handleFileRead} />
+
+
+      
 
       <Routes>
 
         <Route path='*' element={
           <>
           <h1>Biblioteka 2024</h1>
+          {datas}
+          
+          
           <Nav></Nav>
           <Header></Header>
           <Main></Main>
