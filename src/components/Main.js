9a7be25c1bd19a21296fc447414d85e28c12e0f0
marcinoles87 <marcinoles87@gmail.com>
2025-04-18 17:03:09 +0200

    add components

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,12 @@ import React from 'react'
 function Main() {
   return (
     <div className='main-container'>
-      <h1>Najwiekszy zbiór ksiazek w Polsce</h1>
-      <p>Dołącz do naszej spolecznosci juz dzis ...</p>
+
+      <div className='main-info'>
+        <h1>Najwiekszy zbiór ksiazek w Polsce</h1>
+        <p>Dołącz do naszej spolecznosci juz dzis ...</p>
+      </div>
+      
     </div>
   )
 }
