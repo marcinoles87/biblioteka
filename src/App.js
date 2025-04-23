ed11b72b76a5c8d92ccb99d489daf3c34ec32d0b
marcinoles87 <marcinoles87@gmail.com>
2025-04-23 18:11:09 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   return (
     <div className="App">
 
-<input type="file" onChange={handleFileRead} />
+{/* <input type="file" onChange={handleFileRead} /> */}
 
 
       
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function SearchBar() {
+
+  const handleSearch = () => {
+    console.log('jestem')
+  }
+
+
   return (
-    <div>SearchBar</div>
+    <div className='search-container'>
+      <h1>Wyszukiwanie ksiazek</h1>
+      <input placeholder='search by book name'></input>
+      <input placeholder='search by year'></input>
+      <button onClick={handleSearch}>Wyszukaj</button>
+      
+      
+      </div>
   )
 }
 
