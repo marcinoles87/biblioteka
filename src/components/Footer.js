d72cea77ab35e6bb2ab8877a6e5063aa8b7b1936
marcinoles87 <marcinoles87@gmail.com>
2025-05-17 21:29:05 +0200

    add new animation loader

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,28 @@ function Footer() {
   return (
     <div className='footer-container'>
         <div className='footer-info'>
+            <h4>Dane kontaktowe</h4>
             <p>Adres : Wzgorza Krzesławickie 1/1 </p>
             <p>Telefon : 12-645-62-21 </p>
             <p>Email : bibliotekaKrk@gmail.com</p>
         </div>
 
         <div className='footer-info'>
-            <p>Mapa strony</p>
+            <h4>Społeczność</h4>
+            <p>Informacje</p>
+            <p>Dobre rady</p>
+            <p>Blog</p>
+            <p>Strony przyjazne</p>
+            <p>Zaczytany Kraków</p>
+        </div>
+
+        <div className='footer-info'>
+            <h4>Informajce</h4>
             <p>Dostępność</p>
             <p>Dane kontaktowe</p>
+            <p>Polityka Prywatności</p>
+            <p>O Nas</p>
+            <p>FAQ wypożyczania</p>
         </div>
     </div>
   )
