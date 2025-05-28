6cc5fd3ad0e9aea0d9d2d46f4efbdae82e5311af
marcinoles87 <marcinoles87@gmail.com>
2025-05-28 16:33:35 +0200

    add new animation loader

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import logo from '../img/logo.png'
 
 function Nav() {
   return (
     <div className='nav-container' >
-      <h1>Biblioteka Krakowska</h1>
+      <img src={logo} alt=""></img>
       <div className='nav-elements'>
         
         <ul>
