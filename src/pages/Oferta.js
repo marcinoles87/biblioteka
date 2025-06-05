8471691113c3a359e8edc12d344fd6290c597596
marcinoles87 <marcinoles87@gmail.com>
2025-06-05 16:17:44 +0200

    add new component

--- src/pages/Oferta.js
+++ src/pages/Oferta.js
@@ -1,8 +1,47 @@
 import React from 'react'
 
+import zapisy from '../img/zapisy.png'
+
 function Oferta() {
   return (
-    <div>Oferta</div>
+    <div className='oferty-container'>
+
+      <div className='lista-container'>
+
+        <div className='oferta-container'>
+          <img src={zapisy} alt=''></img>
+          <h4>Jak sie zapisać</h4>
+        </div>
+
+        <div className='oferta-container'>
+          <img src={zapisy} alt=''></img>
+          <h4>Jak sie zapisać</h4>
+        </div>
+
+        <div className='oferta-container'>
+          <img src={zapisy} alt=''></img>
+          <h4>Jak sie zapisać</h4>
+        </div>
+
+        <div className='oferta-container'>
+          <img src={zapisy} alt=''></img>
+          <h4>Jak sie zapisać</h4>
+        </div>
+
+        <div className='oferta-container'>
+          <img src={zapisy} alt=''></img>
+          <h4>Jak sie zapisać</h4>
+        </div>
+
+        <div className='oferta-container'>
+          <img src={zapisy} alt=''></img>
+          <h4>Jak sie zapisać</h4>
+        </div>
+
+        
+
+      </div>
+    </div>
   )
 }
 
