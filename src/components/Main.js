b245c8293d9138b6d65df73cddd750b3631c52ec
marcinoles87 <marcinoles87@gmail.com>
2025-06-16 16:30:26 +0200

    add new media

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ function Main() {
     <div className='main-container'>
 
       <div className='main-info'>
-        <p className='main-info-header'>Najwiekszy zbiór książek w Polsce</p>
+        <p className='main-info-header'>Najwiekszy zbiór książek w Małopolsce</p>
         <p className='main-info-text'>Dołącz do naszej spolecznosci juz dzis ...</p>
       </div>
       
