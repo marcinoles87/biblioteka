47b275e31905f49fb5f503394ce5ef0b2ba6511d
marcinoles87 <marcinoles87@gmail.com>
2025-07-01 16:33:29 +0200

    add new css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,14 @@ function Header() {
     <div className='header-container'>
       <div className='header-one'>
         <h1>Relaks</h1>
-        <h2>przy filiżance kawy...</h2>
+        
+        <div className='header-one-text'>
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe tempore laboriosam eum minus itaque vitae consectetur, eaque quibusdam? Deserunt, quos rem aspernatur nobis nam aperiam natus fugiat. Incidunt, minima nisi?
+        Tenetur voluptates iste, harum aliquid sint dolorum doloremque corrupti inventore iure adipisci quasi sed omnis quod iusto, id impedit amet dignissimos sequi facilis odio natus architecto alias ad incidunt? Tenetur.
+        Eum sed quidem, sit reprehenderit quibusdam vitae ex modi aspernatur ipsam nemo, unde, doloribus molestias quod! Facere magni quaerat eum officiis ullam rem ab error, delectus, assumenda veritatis laborum sit.
+        Tenetur voluptates iste, harum aliquid sint dolorum doloremque corrupti inventore iure adipisci quasi sed </p>
+        </div>
+
           
       </div>
 
