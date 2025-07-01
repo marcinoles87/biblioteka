362d1e69151ff95863d36561af6af70d5ab34792
marcinoles87 <marcinoles87@gmail.com>
2025-07-01 16:24:09 +0200

    add new css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import headerOne from '../img/header_one.jpg'
-import headerTwo from '../img/header_two.jpg'
+
 
 function Header() {
   return (
     <div className='header-container'>
       <div className='header-one'>
-          <h1>Kids</h1>
-          {/* <img src={headerOne} alt='header-img'></img> */}
+        <h1>Relaks</h1>
+        <h2>przy filiżance kawy...</h2>
+          
       </div>
 
       <div className='header-two'>
-          <h1>Adult</h1>
-          {/* <img src={headerTwo} alt='header-img'></img> */}
+          
+          
       </div>
 
       
